714a97765c23aec74fdcffe68985f971b68d84ba
Thomas Bullock <motbollox@gmail.com>
2021-01-16 14:54:50 +1100

    Added topping and previews

--- wip/sanity/schemas/pizza.js
+++ wip/sanity/schemas/pizza.js
@@ -1,4 +1,3 @@
-import { rules } from 'eslint-plugin-prettier';
 import { MdLocalPizza as icon } from 'react-icons/md';
 
 export default {
@@ -37,8 +36,35 @@ export default {
             title: 'Price',
             type: 'number',
             description: 'Price of the pizza in cents',
-            cvalidation: Rule => rule.min(1000).max(50000)
+            validation: Rule => Rule.min(1000),
             // Todo: Add custom input component
         },
-    ]
+        {
+            name: 'toppings',
+            title: 'Toppings',
+            type: 'array',
+            of: [{ type: 'reference', to: [{ type: 'topping'}]}]
+        }
+    ],
+    preview: {
+        select: {
+            title: 'name',
+            media: 'image',
+            topping0: 'toppings.0.name',
+            topping1: 'toppings.1.name',
+            topping2: 'toppings.2.name',
+            topping3: 'toppings.3.name',
+        },
+        prepare: ({ title, media, ...toppings}) => {
+            // 1. Filter undefined toppings out
+            const tops  = Object.values(toppings).filter(Boolean)
+            // 2. return the preview
+
+            return {
+                title, 
+                media,
+                subtitle: tops.join(',')
+            }
+        }
+    }
 };
