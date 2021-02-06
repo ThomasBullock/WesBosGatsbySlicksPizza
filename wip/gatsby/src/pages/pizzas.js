3509ca46b6ac5ab60e526b3c0e7ddc8daeb14919
Thomas Bullock <motbollox@gmail.com>
2021-02-06 11:50:18 +1100

    Added images and grid

--- wip/gatsby/src/pages/pizzas.js
+++ wip/gatsby/src/pages/pizzas.js
@@ -1,7 +1,39 @@
+import { graphql } from 'gatsby';
 import React, { Fragment } from 'react';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+    console.log(data.pizzas)
+    const pizzas = data.pizzas.nodes
     return (<Fragment>
-        <p>Hey! I'm the pizza page</p>
+        <PizzaList pizzas={pizzas}/>
     </Fragment>);
 }
+
+export const query =  graphql`
+    query PizzaQuery {
+    pizzas: allSanityPizza {
+        nodes {
+        name
+        id
+        slug {
+            current
+        }
+        toppings {
+            id
+            name
+        }
+        image {
+            asset {
+                fixed(width: 200, height: 200) {
+                        ...GatsbySanityImageFixed
+                }
+                fluid(maxWidth: 400) {
+                        ...GatsbySanityImageFluid
+                }
+            }
+        }
+        }
+    }
+    }
+`  
