6d9e8d8c59e5fb938e736200504ecee88558ea6b
Thomas Bullock <motbollox@gmail.com>
2021-02-26 19:11:32 +1100

    Order Styles

--- wip/gatsby/src/pages/order.js
+++ wip/gatsby/src/pages/order.js
@@ -5,6 +5,8 @@ import useForm from "../utils/useForm";
 import Img from "gatsby-image";
 import calculatePizzaPrice from "../utils/calculatePizzaPrice";
 import formatMoney from "../utils/formatMoney";
+import OrderStyles from "../styles/OrderStyles";
+import MenuItemStyles from "../styles/MenuItemStyles";
 
 export default function OrderPage({ data }) {
     const { values, updateValue } = useForm({
@@ -16,7 +18,7 @@ export default function OrderPage({ data }) {
     return (
         <Fragment>
             <SEO title="Order a Pizza!" />
-            <form>
+            <OrderStyles>
                 <fieldset>
                     <legend>Your Info</legend>
                     <label htmlFor="name">Name</label>
@@ -24,10 +26,10 @@ export default function OrderPage({ data }) {
                     <label htmlFor="email">Email</label>
                     <input type="text" name="email" id="email" value={values.email} onChange={updateValue} />
                 </fieldset>
-                <fieldset>
+                <fieldset className="menu">
                     <legend>Menu</legend>
                     {pizzas.map((pizza) => (
-                        <div key={pizza.id}>
+                        <MenuItemStyles key={pizza.id}>
                             <Img width="50" height="50" fluid={pizza.image.asset.fluid} alt={pizza.name} />
                             <div>
                                 <h2>{pizza.name}</h2>
@@ -39,13 +41,13 @@ export default function OrderPage({ data }) {
                                     </button>
                                 ))}
                             </div>
-                        </div>
+                        </MenuItemStyles>
                     ))}
                 </fieldset>
-                <fieldset>
+                <fieldset className="order">
                     <legend>Order</legend>
                 </fieldset>
-            </form>
+            </OrderStyles>
         </Fragment>
     );
 }
