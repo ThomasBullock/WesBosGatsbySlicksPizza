1567873965ac8a1cf4e45de9e2ea1011cf64371a
Thomas Bullock <motbollox@gmail.com>
2021-03-03 18:36:34 +1100

    Finished server function

--- wip/gatsby/functions/placeOrder/placeOrder.js
+++ wip/gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,26 @@
 const nodemailer = require("nodemailer");
 
+function generateOrderEmail({ order, total }) {
+    return `<div>
+        <h2>Your Recent Order for ${total}</h2>
+        <p>Please start walking over, we will have your order ready in the next 20mins.</p>
+        <ul>
+        ${order.map(
+            (item) => `<li>
+            <img src="${item.thumbnail}" alt="${item.name}"/>
+            ${item.size} ${item.name} - ${item.price}
+        </li>`
+        )}
+        </ul>
+        <p>Your total is <strong>${total}</strong> due at pickup.</p>
+        <style>
+            ul {
+                list-style: none
+            }
+        </style>
+    </div>`;
+}
+
 // create a transport for nodemailer
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
@@ -11,16 +32,36 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
+    const body = JSON.parse(event.body);
+    // Validate teh datq coming in si correct
+    const requiredFields = ["email", "name", "order"];
+
+    for (const field of requiredFields) {
+        console.log(`Checking that ${field} is goood`);
+        if (!body[field]) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: `Ooops! You are missing the ${field} field`,
+                }),
+            };
+        }
+    }
+
+    // send the email
+
+    // send succes or error
+
     // Test send an email
     const info = await transporter.sendMail({
         from: "Slick's Slices <slick@example.com>",
-        to: "orders@example.com",
+        to: `${body.name} <${body.email}>, orders@example.com`,
         subject: "New Order",
-        html: `<p>Your new pizza order is here!</p>`,
+        html: generateOrderEmail({ order: body.order, total: body.total }),
     });
 
     return {
         statusCode: 200,
-        body: JSON.stringify(info),
+        body: JSON.stringify({ message: "Success" }),
     };
 };
